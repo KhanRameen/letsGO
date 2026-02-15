30353179f4e635bc80061457453b0dc1f6c5d519
Rameen Khan <rameenk181@gmail.com>
2026-02-15 22:24:57 +0500

    "worker-pool: add retries"

--- builds/worker-pool/worker.go
+++ builds/worker-pool/worker.go
@@ -6,19 +6,29 @@ import (
 	"sync"
 )
 
+ var maxRetries = 3
+
 func worker(id int, jobs <-chan string, results chan<- string, wg *sync.WaitGroup) {  // worker receives jobs: <-chan string, wokder sends results: chan<- string
 	defer wg.Done() //decrement the wait group counter when the worker is done
 
 	for url := range jobs { //range over the jobs channel to receive jobs until it's closed{}
-		fmt.Printf("Worker %d checking %s\n", id, url) //%d:int , %s:string
-
+	retries := 0
+	
+	for{
+		fmt.Printf("Worker %d checking %s\n Attempt %d", id, url, retries) //%d:int , %s:string	
+		
 		resp , err := http.Get(url) //make an HTTP GET request to the URL
 		if err != nil {
-			results <- fmt.Sprintf("Worker %d: Error checking %s: %v", id, url, err) //send error message to results channel
+			retries++
+			if retries >= maxRetries {
+				results <- fmt.Sprintf("Worker %d: Error checking %s: %v", id, url, err) //send error message to results channel
+				break
+			}
 			continue
-		} 
-
-		resp.Body.Close() //close the response body to free up resources
-		results <- fmt.Sprintf("Worker %d: %s is up! Status Code: %d", id, url, resp.StatusCode) //send success message to results channel
+			} 			
+			resp.Body.Close() //close the response body to free up resources
+			results <- fmt.Sprintf("Worker %d: %s is up! Status Code: %d", id, url, resp.StatusCode) //send success message to results channel
+			break
+		}
 	}
-}
+}		
